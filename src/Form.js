fc37ba0971700c7a08265f4c48f092ab601751a8
Michael Le-Reiver <m.lereiver@gmail.com>
2018-06-27 11:18:17 -0700

    created static versoin of form

---
+++ src/Form.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {reduxForm, focus} from 'redux-form';
+
+
+export class TrackingForm extends React.Component {
+ 
+  
+
+
+  render() {
+    return (
+      <div>
+        <h2>Report a problem with your delivery</h2>
+        <form>
+          <label for='tracking-number-input'>Tracking Number</label>
+          <input 
+            type='text' 
+            label='tracking-number' 
+            name='tracking-number-input'
+          />
+          <label for='issue-selector'>What is your issue?</label>
+          <select name="issue-selector" id="issue">
+          <option value="not-delivered">My delivery hasn't arrived</option>
+          <option value="wrong-item">The wrong item was delivered</option>
+          <option value="missing-part">Part of my order was missing</option>
+          <option value="damaged">Some of my order arrived damaged</option>
+          <option value="other">Other (give details below)</option>
+          </select>
+          <div class="form-input">
+            <label for="details">Give more details (optional)</label>
+            <textarea name="details" id="details"></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+
+  }
+}
+
+  export default reduxForm({
+    form: 'contact',
+    onSubmitFail: (errors, dispatch) =>
+      dispatch(focus('contact', Object.keys(errors)[0]))
+  })(TrackingForm);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
-import App from './App';
+import store from './store';
+import TrackingForm from './Form';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <TrackingForm />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
+
